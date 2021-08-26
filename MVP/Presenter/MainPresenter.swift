df14d115ee6dbb5c2df48b5a19ded7a2429c2c3b
Vladislav <lordroga2@gmail.com>
2021-08-26 21:04:55 +0300

    added presenter

--- MVP/Presenter/MainPresenter.swift
+++ MVP/Presenter/MainPresenter.swift
@@ -7,3 +7,30 @@
 
 import Foundation
 
+protocol MainViewProtocol {
+    func setGreeting(greeting: String)
+}
+
+protocol MainViewPresenterProtocol {
+    init(view: MainViewProtocol, person: Person)
+    
+    func showGreeting()
+}
+
+class MainPresenter: MainViewPresenterProtocol {
+    let view: MainViewProtocol!
+    let person: Person
+    
+    required init(view: MainViewProtocol, person: Person) {
+        self.view = view
+        self.person = person
+    }
+    
+    func showGreeting() {
+        let greeting = self.person.firstName + " " + self.person.lastName
+        self.view.setGreeting(greeting: greeting)
+    }
+    
+    
+    
+}
