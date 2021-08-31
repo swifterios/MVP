c501ac9a8f9bb95d6ec45756ad260f3304ea0bdc
Vladislav <lordroga2@gmail.com>
2021-08-31 23:17:27 +0300

    added unit test isNil

--- MVPTests/TestMainModule/MainPresenterTest.swift
+++ MVPTests/TestMainModule/MainPresenterTest.swift
@@ -5,5 +5,39 @@
 //  Created by Владислав on 31.08.2021.
 //
 
-import Foundation
+import XCTest
+@testable import MVP
 
+class MockView: MainViewProtocol {
+    var titleTest: String?
+    func setGreeting(greeting: String) {
+        self.titleTest = greeting
+    }
+}
+
+class MainPresenterTest: XCTestCase {
+
+    var view: MockView!
+    var person: Person!
+    var presenter: MainPresenter!
+    
+    
+    override func setUpWithError() throws {
+        view = MockView()
+        person = Person(firstName: "Baz", lastName: "Bar")
+        presenter = MainPresenter(view: view, person: person)
+    }
+
+    override func tearDownWithError() throws {
+        view = nil
+        person = nil
+        presenter = nil
+    }
+    
+    func testModuleIsNotNil() {
+        XCTAssertNotNil(view, "View is not nil")
+        XCTAssertNotNil(person, "Person is not nil")
+        XCTAssertNotNil(presenter, "Presenter is not nil")
+    }
+
+}
